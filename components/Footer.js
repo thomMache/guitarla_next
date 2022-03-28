6829069e6342f31d63359e8bbe37d25944398a8c
thom_mache <thomas.ubuntu.lik@gmail.com>
2022-03-27 19:25:46 -0600

    Links de inico nosotros blog y tienda

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
 import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
 
 const Footer = () =>{
     return (
-        <footer>
-            <div className="contenedor">
-            <nav className=''>
+        <footer className={styles.footer}>
+            <div className={`contenedor ${styles.contenido}`}>
+            <nav className={styles.navegacion}>
                     <Link href ="/">Inicio</Link>
                     <Link href ="/nosotros">Nosotros</Link>
                     <Link href ="/blog">Blog</Link>
                     <Link href ="/tienda">Tienda</Link>
                 </nav>
-
-                <p>Todos los derechos reservados</p>
+                <p className={styles.copyright}>Todos los derechos reservados</p>
             </div>
         </footer>
     )
